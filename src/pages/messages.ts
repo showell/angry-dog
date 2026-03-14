fa3c5941421e1eef6b10899fabfc08419a7b8b55
Steve Howell <showell30@yahoo.com>
2026-03-14 07:13:55 -0400

    minor cleanup

--- src/pages/messages.ts
+++ src/pages/messages.ts
@@ -1,24 +1,39 @@
+import he from "he";
+
 import { MessageRow } from "../backend/row_types";
 
 import { DB } from "../backend/database";
 
+const STYLE = `
+<style>
+    .sender {
+        font-weight: bold;
+    }
+</style>
+`;
+
+export function message_html(message_row: MessageRow): string {
+    const sender_name = he.escape(message_row.sender_name());
+    const content = message_row.content();
+    return `
+<div class="sender">${sender_name}</div>
+<div>${content}</div>
+<hr />
+`;
+}
+
 export function html(): string {
     const messages = [...DB.message_map.values()];
 
-    messages.sort((m1, m2) => m2.id - m1.id);
+    messages.sort((m1, m2) => m1.id - m2.id);
 
-    let html = "";
+    let html = STYLE;
 
     html += `${messages.length} messages</h4>`;
 
     for (const message of messages) {
         const message_row = new MessageRow(message);
-
-        const content = message_row.content();
-
-        html += `<div>${message_row.sender_name()}</div>`;
-        html += `<div>${content}</div>`;
-        html += `<hr>`;
+        html += message_html(message_row);
     }
 
     return html;
