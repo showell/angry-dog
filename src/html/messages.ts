4972693228c6258f8c6220ada6ef8bd8eaa2b0bc
Steve Howell <showell30@yahoo.com>
2026-03-18 12:12:56 -0400

    cleanup

--- src/html/messages.ts
+++ src/html/messages.ts
@@ -2,31 +2,43 @@ import he from "he";
 
 import { DB } from "../database/database";
 
-export function message_html(message_id: number): string {
+function get_topic_name(channel_topic_id: number): string {
+    const topic_id = DB.channel_topic.get_id2(channel_topic_id)!;
+    return DB.topic_name.get_string(topic_id) ?? "unknown";
+}
+
+function get_message_ids(channel_topic_id: number): number[] {
+    return [...DB.message_to_channel_topic.reverse_get(channel_topic_id)];
+}
+
+function get_sender_name(message_id: number): string {
     const sender_id = DB.message_sender.get(message_id)!;
-    const sender_name = he.escape(
-        DB.user_full_name.get_string(sender_id) ?? "unknown",
-    );
+    return DB.user_full_name.get_string(sender_id) ?? "unknown";
+}
+
+function get_content(message_id: number): string {
     const angry_dog_content_id = DB.message_content.get(message_id)!;
-    const content = DB.content_string.get_string(angry_dog_content_id)!;
+    return DB.content_string.get_string(angry_dog_content_id)!;
+}
+
+export function message_html(message_id: number): string {
+    const sender_name = get_sender_name(message_id);
+    const safe_html_content = get_content(message_id);
 
     return `
-<div class="message_sender">${sender_name}</div>
-<div>${content}</div>
+<div class="message_sender">${he.escape(sender_name)}</div>
+<div>${safe_html_content}</div>
 <hr />
 `;
 }
 
 export function by_topic_html(channel_topic_id: number): string {
-    const message_ids = [
-        ...DB.message_to_channel_topic.reverse_get(channel_topic_id),
-    ];
+    const message_ids = get_message_ids(channel_topic_id);
     message_ids.sort();
 
-    const topic_id = DB.channel_topic.get_id2(channel_topic_id)!;
-    const topic_name = he.escape("> " + DB.topic_name.get_string(topic_id));
+    const topic_name = get_topic_name(channel_topic_id);
 
-    let html = `<h4>${message_ids.length} messages for ${topic_name}</h4>`;
+    let html = `<h4>${message_ids.length} messages for ${"> " + he.escape(topic_name)}</h4>`;
 
     for (const message_id of message_ids) {
         html += message_html(message_id);
