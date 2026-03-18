6e98b26b66c5450d025eea4c755caef44af41b6c
Steve Howell <showell30@yahoo.com>
2026-03-18 09:34:05 -0400

    EVENTS: Remove all event code for now!!!!

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,30 +1,12 @@
-import type { ZulipEvent } from "./event";
-
 import * as database from "../database/database";
-import * as event_glue from "../database/event_glue";
 
-import { EventHandler } from "./event";
 import * as channel_fetch from "./channel_fetch";
 import * as message_fetch from "./message_fetch";
 import * as zulip_client from "./zulip_client";
 
 export async function start() {
-    function handle_zulip_event(event: ZulipEvent) {
-        event_glue.handle_event(event);
-    }
-
-    const event_manager = new EventHandler(handle_zulip_event);
-
-    // we wait for register to finish, but then polling goes
-    // on "forever" asynchronously
-    await zulip_client.register_queue();
-
     database.initialize_DB();
-
     await channel_fetch.fetch_channel_data();
     await message_fetch.fetch_initial_messages();
-
-    zulip_client.start_polling(event_manager);
-
     message_fetch.backfill();
 }
--- src/client/zulip_client.ts
+++ src/client/zulip_client.ts
@@ -1,54 +1,9 @@
-import type { EventHandler } from "./event";
-
 import { TEST_CONFIG } from "../test_config";
-
-let queue_id: string | undefined;
-let last_event_id: string | undefined;
 
 function get_headers() {
     const auth = btoa(`${TEST_CONFIG.email}:${TEST_CONFIG.api_key}`);
     const auth_header = `Basic ${auth}`;
     return { Authorization: auth_header };
-}
-
-export async function register_queue() {
-    const url = new URL("/api/v1/register", TEST_CONFIG.url);
-    url.searchParams.set("apply_markdown", "true");
-    url.searchParams.set("include_subscribers", "false");
-    url.searchParams.set("slim_presence", "true");
-    url.searchParams.set("all_public_streams", "false");
-
-    const response = await fetch(url, {
-        method: "POST",
-        headers: get_headers(),
-    });
-    const data = await response.json();
-    queue_id = data.queue_id;
-    last_event_id = data.last_event_id;
-}
-
-export async function start_polling(event_handler: EventHandler) {
-    if (queue_id === undefined || last_event_id === undefined) {
-        return;
-    }
-
-    const url = new URL("/api/v1/events", TEST_CONFIG.url);
-
-    while (queue_id !== undefined && last_event_id !== undefined) {
-        url.searchParams.set("queue_id", queue_id);
-        url.searchParams.set("last_event_id", last_event_id);
-
-        const response = await fetch(url, { headers: get_headers() });
-        const data = await response.json();
-
-        if (data.result !== "success") {
-            window.location.reload();
-        }
-        if (data.events?.length) {
-            last_event_id = data.events[data.events.length - 1].id;
-            event_handler.process_events(data.events);
-        }
-    }
 }
 
 export async function get_messages(anchor: string, num_before: number) {
