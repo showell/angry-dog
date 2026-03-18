b1a59689665dc3f904edcd3f3aca66a98c6c1857
Steve Howell <showell30@yahoo.com>
2026-03-18 11:53:43 -0400

    cleanup

--- src/html/channels.ts
+++ src/html/channels.ts
@@ -1,14 +1,26 @@
 import he from "he";
-
 import { DB } from "../database/database";
+import { alpha_sort } from "../util/sort";
+
+function get_channel_ids(): number[] {
+    return DB.channel_name.id_array();
+}
+
+function get_channel_name(channel_id: number): string {
+    return DB.channel_name.get_string(channel_id) ?? "unknown";
+}
+
+function get_topic_count(channel_id: number): number {
+    return DB.channel_topic.get_id2_count(channel_id);
+}
 
 function channel_row_html(channel_id: number): string {
-    const name = he.escape(DB.channel_name.get_string(channel_id) ?? "unknown");
-    const topic_count = DB.channel_topic.get_id2_count(channel_id);
+    const name = get_channel_name(channel_id);
+    const topic_count = get_topic_count(channel_id);
 
     return `
 <div class="channel_row">
-    <div class="channel_name">${name}</div>
+    <div class="channel_name">${he.escape(name)}</div>
     <div>
         <a href="/topics/${channel_id}">topics</a>
     </div>
@@ -20,11 +32,7 @@ function channel_row_html(channel_id: number): string {
 export function html(): string {
     const channel_ids = DB.channel_name.id_array();
 
-    channel_ids.sort((id1, id2) => {
-        const name1 = DB.channel_name.get_string(id1) ?? "unknown";
-        const name2 = DB.channel_name.get_string(id2) ?? "unknown";
-        return name1.localeCompare(name2);
-    });
+    alpha_sort(channel_ids, get_channel_name);
 
     let html = `<h4>${channel_ids.length} channels</h4>`;
 
